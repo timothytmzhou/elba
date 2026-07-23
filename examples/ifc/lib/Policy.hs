5232ae4a22ac2f1580e605e38d28bde524e5381c
timothytmzhou <timothytmzhou@gmail.com>
2026-07-22 21:45:23 -0700

    delete dead definitions

--- examples/ifc/lib/Policy.hs
+++ examples/ifc/lib/Policy.hs
@@ -6,34 +6,14 @@ module Policy
   , assertWrite
   , assertIntegrity
   , write
-  , public
-  , secret
-  , trusted
-  , untrusted
   ) where
 
 import Control.Monad (unless)
 import LIO (getLabel, speaksFor)
-import LIO.DCLabel (CNF, DC, DCLabel, DCLabeled, cFalse, cTrue, dcIntegrity, toCNF, (%%))
+import LIO.DCLabel (CNF, DC, DCLabel, DCLabeled, cTrue, dcIntegrity, toCNF, (%%))
 import LIO.Error (labelError)
 import LIO.Labeled (unlabelP)
 import LIO.TCB (Priv (PrivTCB), ioTCB)
-
--- | Secrecy anyone may read.
-public :: CNF
-public = cTrue
-
--- | Secrecy nobody may read.
-secret :: CNF
-secret = cFalse
-
--- | Integrity endorsed.
-trusted :: CNF
-trusted = cFalse
-
--- | Integrity unendorsed.
-untrusted :: CNF
-untrusted = cTrue
 
 unlabelTCB :: DCLabeled a -> DC a
 unlabelTCB = unlabelP (PrivTCB (toCNF False))
